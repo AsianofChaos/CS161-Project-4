c085ca9448ceaa897da9aae09091fb517d606199
Joshua Isakson <Joshuabisakson@gmail.com>
2021-06-13 22:04:35 -0700

    yes

--- CS-161-Project-4.py
+++ CS-161-Project-4.py
@@ -2,11 +2,19 @@
 #6/5/2021
 
 
-class Myclass: 
-    x=5
+class Car: 
+    def __init__ (self, color, speed, brand, model):
+        self.brand = brand 
+        self.color = color 
+        self.speed = speed 
+        self.model = model
 
 
 
-s = Myclass()
 
-print(s.x)
+s = Car(1,2,3,4)
+
+print(s.brand)
+print(s.model)
+print(s.speed)
+print(s.color)
